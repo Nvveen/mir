4517a637963325e1a65e0f9fe0813b5a7f9135fc
Neal van Veen <nealvanveen@gmail.com>
2015-03-22 22:14:15 +0100

    fix addurl for duplicates, add geturl method

--- tree/urllist.go
+++ tree/urllist.go
@@ -1,21 +1,44 @@
 package tree
 
-import "net/url"
+import (
+	"errors"
+	"net/url"
+)
 
 // TODO maybe change package name to something more general than tree
 // TODO add benchmarks
+// TODO add URLList errors
 
 type URLList []*url.URL
 
 // Create a new URLList object.
-func NewURLList() (list *URLList) {
-	return &URLList{}
+func NewURLList() (list URLList) {
+	return URLList{}
 }
 
 // Add a new URL object to the list.
 func (u *URLList) AddURL(url *url.URL) (err error) {
 	defer func() {
-		err = recover()
+		if vErr := recover(); vErr != nil {
+			err = errors.New("could not add URL to list")
+		}
 	}()
+	for i := range *u {
+		if *((*u)[i]) == *url {
+			return
+		}
+	}
 	*u = append(*u, url)
+	return
+}
+
+// Retrieve a URL from its index.
+func (u *URLList) GetURL(i int) (url *url.URL, err error) {
+	defer func() {
+		if dErr := recover(); dErr != nil {
+			err = errors.New("could not retrieve URL from list")
+		}
+	}()
+	url = (*u)[i]
+	return
 }
--- tree/urllist_test.go
+++ tree/urllist_test.go
@@ -1,6 +1,48 @@
 package tree
 
-func TestURLList_AddURL(t *testing.T) {
+import (
+	"errors"
+	"net/url"
+	"testing"
+)
+
+func makeTestList(t *testing.T) URLList {
 	l := NewURLList()
-	l.AddURL("http://www.google.com")
+	str := []*url.URL{
+		&url.URL{Scheme: "http", Host: "www.google.com", Path: "/"},
+		&url.URL{Scheme: "http", Host: "www.liacs.nl", Path: "/"},
+	}
+	err := l.AddURL(str[0])
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = l.AddURL(str[1])
+	if err != nil {
+		t.Fatal(err)
+	}
+	return l
+}
+
+func TestURLList_AddURL(t *testing.T) {
+	str := []*url.URL{
+		&url.URL{Scheme: "http", Host: "www.google.com", Path: "/"},
+		&url.URL{Scheme: "http", Host: "www.liacs.nl", Path: "/"},
+	}
+	l := makeTestList(t)
+	for i := range str {
+		if str[i].String() != l[i].String() {
+			t.Fatal(errors.New("Adding URLS to URLList failure"))
+		}
+	}
+}
+
+func TestURLList_GetURL(t *testing.T) {
+	l := makeTestList(t)
+	url, err := l.GetURL(0)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if url.String() != "http://www.google.com/" {
+		t.Fatal(errors.New("Invalid URL retrieve from URLList"), ":", url)
+	}
 }
