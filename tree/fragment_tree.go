cd74a28b403daf4d73f421f93016690804e181e9
Neal van Veen <nealvanveen@gmail.com>
2015-03-22 02:04:28 +0100

    add fragmenttree

---
+++ tree/fragment_tree.go
@@ -0,0 +1,94 @@
+package tree
+
+import (
+	"errors"
+	"net/url"
+	"strings"
+)
+
+// TODO implement tokenization errors
+// TODO implement fragmenttree errors
+
+// The data structure that allows for lower storage requirements by
+// fragmenting urls and storing each fragment as a key. For this to be
+// succesfull, a large number of links need to stored (compared to a
+// simple string list.
+type FragmentTree struct {
+	root *node
+}
+
+// A simple node structure used by the FragmentTree data structure.
+type node struct {
+	label    string
+	children []*node
+}
+
+// Make a new FragmentTree object.
+func NewFragmentTree() (t *FragmentTree, err error) {
+	return &FragmentTree{}, nil
+}
+
+// Add a url by fragmenting it and using each fragment as a key in
+// a tree.
+func (f *FragmentTree) AddURL(url *url.URL) (err error) {
+	words, err := TokenizeURL(url)
+	if err != nil {
+		return
+	}
+	if f.root == nil {
+		f.root = new(node)
+	}
+	_ = words
+	var fn func(p *node, parent *node, i int)
+	fn = func(p *node, parent *node, i int) {
+		if i > len(words)-1 {
+			return
+		}
+		for j := range p.children {
+			if p.children[j] != nil && p.children[j].label == words[i] {
+				fn(p.children[j], p, i+1)
+				return
+			}
+		}
+		// This part does not exist
+		c := p
+		for j := i; j < len(words); j++ {
+			c.children = append(c.children, new(node))
+			c.children[len(c.children)-1].label = words[j]
+			c = c.children[len(c.children)-1]
+		}
+	}
+	fn(f.root, f.root, 0)
+	return
+}
+
+// Tokenize an URL into separate alphanumeric words for indexing purposes.
+func TokenizeURL(url *url.URL) (tok []string, err error) {
+	if url.User != nil || len(url.Opaque) > 0 {
+		err = errors.New("URL Tokenizer: Not a simple URL")
+		return
+	}
+	tok = append(tok, strings.Split(url.Host, ".")...)
+	// Split path
+	path := url.Path
+	path = strings.TrimSpace(path)
+	if path != "/" {
+		if strings.HasPrefix(path, "/") {
+			path = path[1:]
+		}
+		if strings.HasSuffix(path, "/") {
+			path = path[:len(path)-1]
+		}
+		tok = append(tok, strings.Split(path, "/")...)
+	}
+	// Parse query
+	values := url.Query()
+	for key, val := range values {
+		tok = append(tok, key)
+		for i := range val {
+			tok = append(tok, val[i])
+		}
+	}
+	// Now all words in a string have been appended to the string
+	return
+}
---
+++ tree/fragment_tree_test.go
@@ -0,0 +1,91 @@
+package tree
+
+import (
+	"errors"
+	"net/url"
+	"testing"
+)
+
+// TODO add examples in a new file
+
+func printTokens(t *testing.T, toks []string) {
+	str := "{"
+	for i := range toks {
+		str += toks[i]
+		if i < len(toks)-1 {
+			str += ","
+		}
+	}
+	t.Log(str, "}")
+}
+
+func TestTokenizeURL(t *testing.T) {
+	u, err := url.Parse("http://www.google.com/?q=1&q=twee&p=wat")
+	if err != nil {
+		t.Fatal(err)
+	}
+	words, err := TokenizeURL(u)
+	if err != nil {
+		t.Fatal(err)
+	}
+	compare := []string{"www", "google", "com", "q", "1", "twee", "p", "wat"}
+	if len(words) != len(compare) {
+		printTokens(t, words)
+		printTokens(t, compare)
+		t.Fatal("URL Tokenize: Invalid tokenization result")
+	}
+	for i := range words {
+		if words[i] != compare[i] {
+			printTokens(t, words)
+			printTokens(t, compare)
+			t.Fatal("URL Tokenize: Invalid tokenization result")
+		}
+	}
+}
+
+func TestNewTree(t *testing.T) {
+	_, err := NewFragmentTree()
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestFragmentTree_AddURL(t *testing.T) {
+	f, err := NewFragmentTree()
+	if err != nil {
+		t.Fatal(err)
+	}
+	u, err := url.Parse("http://www.google.com")
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = f.AddURL(u)
+	if err != nil {
+		t.Fatal(err)
+	}
+	u, err = url.Parse("http://www.leidenuniv.nl")
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = f.AddURL(u)
+	if err != nil {
+		t.Fatal(err)
+	}
+	assert := func(condition bool) {
+		if !condition {
+			t.Fatal(errors.New("Invalid fragment tree"))
+		}
+	}
+	assert(f.root != nil && f.root.label == "")
+	assert(
+		f.root.children[0] != nil &&
+			f.root.children[0].label == "www" &&
+			f.root.children[0].children[0] != nil &&
+			f.root.children[0].children[0].label == "google" &&
+			f.root.children[0].children[1] != nil &&
+			f.root.children[0].children[1].label == "leidenuniv" &&
+			f.root.children[0].children[0].children[0] != nil &&
+			f.root.children[0].children[0].children[0].label == "com" &&
+			f.root.children[0].children[1].children[0] != nil &&
+			f.root.children[0].children[1].children[0].label == "nl")
+}
