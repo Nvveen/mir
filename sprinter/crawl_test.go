21ed0e527b7f60a10525a644562b3f7f1c301710
Neal van Veen <nealvanveen@gmail.com>
2015-04-20 16:56:50 +0200

    fix crawl_test

--- sprinter/crawl_test.go
+++ sprinter/crawl_test.go
@@ -1,7 +1,9 @@
 package sprinter_test
 
 import (
+	"bytes"
 	"errors"
+	"fmt"
 	"io"
 	"net/http"
 	"net/url"
@@ -31,19 +33,50 @@ var (
 	link = "http://www.liacs.nl"
 
 	c *Crawler
+	m *mockStorage
 )
 
-type mockStorage struct{}
+type mockStorage map[string]map[string][]string
 
-func (m mockStorage) OpenConnection() error {
+func (m *mockStorage) OpenConnection() error {
 	return nil
 }
 
-func (m mockStorage) CloseConnection() {
+func (m *mockStorage) CloseConnection() {
 }
 
-func (m mockStorage) InsertRecord(key string, url string, collection string) error {
+func (m *mockStorage) InsertRecord(key string, url string, collection string) (err error) {
+	defer func(err *error) {
+		if r := recover(); r != nil {
+			*err = errors.New("could not insert into mock storage")
+		}
+	}(&err)
+	// ignore duplicates
+	for i := range (*m)[collection][key] {
+		if (*m)[collection][key][i] == url {
+			return nil
+		}
+	}
+	(*m)[collection][key] = append((*m)[collection][key], url)
 	return nil
+}
+
+func (m mockStorage) String() string {
+	buf := new(bytes.Buffer)
+	for col, _ := range m {
+		fmt.Fprintf(buf, "%s:\n", col)
+		for key, val := range m[col] {
+			fmt.Fprintf(buf, "\t%s: ", key)
+			for i := range val {
+				if i < len(val)-1 {
+					fmt.Fprintf(buf, "%s, ", val[i])
+				} else {
+					fmt.Fprintf(buf, "%s\n", val[i])
+				}
+			}
+		}
+	}
+	return buf.String()
 }
 
 // We need to wrap a stringbuffer to be a ReadCloser
@@ -56,8 +89,16 @@ func (s strBufCloser) Close() error {
 }
 
 func makeCrawler(t *testing.T) (cr *Crawler) {
+	if m == nil {
+		m = new(mockStorage)
+	}
+	*m = mockStorage{
+		"urlindex":  map[string][]string{},
+		"linkindex": map[string][]string{},
+		"wordindex": map[string][]string{},
+	}
 	if c == nil {
-		cr, err := NewCrawler(&containers.List{}, mockStorage{})
+		cr, err := NewCrawler(&containers.List{}, m)
 		if err != nil {
 			t.Fatal(err)
 		}
@@ -71,8 +112,10 @@ func makeCrawler(t *testing.T) (cr *Crawler) {
 }
 
 func TestNewCrawler(t *testing.T) {
+	// No need for making a crawler with the correct mockStorage, as we
+	// won't be using any functionality in this test.
 	l := &containers.List{}
-	_, err := NewCrawler(l, mockStorage{})
+	_, err := NewCrawler(l, &mockStorage{})
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -80,14 +123,14 @@ func TestNewCrawler(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	_, err = NewCrawler(b, mockStorage{})
+	_, err = NewCrawler(b, &mockStorage{})
 	if err != nil {
 		t.Fatal(err)
 	}
 }
 
 func TestSetURL(t *testing.T) {
-	c, err := NewCrawler(&containers.List{}, mockStorage{})
+	c, err := NewCrawler(&containers.List{}, &mockStorage{})
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -98,47 +141,29 @@ func TestSetURL(t *testing.T) {
 }
 
 func TestCrawler_GetURL(t *testing.T) {
-	c, err := NewCrawler(&containers.List{}, mockStorage{})
-	if err != nil {
-		t.Fatal(err)
-	}
-	err = c.AddURL("http://www.google.com")
-	if err != nil {
-		t.Fatal(err)
-	}
+	c := makeCrawler(t)
 	result, err := c.GetURL(0)
 	if err != nil {
 		t.Fatal(err)
 	}
-	if result != "http://www.google.com" {
-		t.Fatal("invalid URL returned")
+	if result != u.String() {
+		t.Fatalf("invalid URL returned: %s", result)
 	}
 }
 
 func TestRetrieveHTML(t *testing.T) {
-	c, err := NewCrawler(&containers.List{}, mockStorage{})
-	if err != nil {
-		t.Fatal(err)
-	}
-	c.AddURL("http://www.google.com")
+	c := makeCrawler(t)
 	result, err := c.RetrieveHTML(0)
 	if err != nil {
 		t.Fatal(err)
 	} else if len(result) == 0 {
-		t.Fatal("no response from http://www.google.com")
+		t.Fatal("no response from " + u.String())
 	}
 }
 
 func TestCrawler_ExtractInfo(t *testing.T) {
-	c, err := NewCrawler(&containers.List{}, mockStorage{})
-	if err != nil {
-		t.Fatal(err)
-	}
-	err = c.AddURL("http://www.leidenuniv.nl")
-	if err != nil {
-		t.Fatal(err)
-	}
-	err = c.ExtractInfo(0)
+	c := makeCrawler(t)
+	err := c.ExtractInfo(0)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -188,4 +213,5 @@ func TestCrawler_Index(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
+	t.Logf("\n%s", m)
 }
