829239c57dca79b495edbc52000c161c338aa5c8
Neal van Veen <nealvanveen@gmail.com>
2015-04-30 13:23:43 +0200

    redo first steps (linkindex) of crawler

--- sprinter/crawl.go
+++ sprinter/crawl.go
@@ -4,293 +4,131 @@ package sprinter
 import (
 	"crypto/md5"
 	"encoding/hex"
+	"errors"
 	"fmt"
-	"io/ioutil"
 	"net/http"
 	"net/url"
-	"regexp"
-	"strings"
+	"time"
 
-	"github.com/Nvveen/mir/containers"
-	"github.com/temoto/robotstxt-go"
 	"golang.org/x/net/html"
 )
 
-// A web crawler structure that stores information on what pages
-// it visits.
 type Crawler struct {
-	db      Storage
-	urlList containers.Container
-	robots  map[string]*robotstxt.RobotsData
-	links   chan string
+	links                 chan string
+	MaxRequests           int
+	MaxConcurrentRequests int
+	db                    Storage
 }
 
 var (
-	ErrAddURL         = CrawlerError("unable to add URL")
-	ErrNewCrawler     = CrawlerError("failed to make a Crawler object")
-	ErrInvalidElement = CrawlerError("invalid element in container")
-	ErrAccessDenied   = CrawlerError("not allowed to crawl this subdomain")
-
-	SkippedFragments = []string{
-		"i", "a", "the",
-	}
+	ErrInvalidParameters = errors.New("invalid parameters for crawler")
 )
 
-const (
-	RobotsSize        = 2000
-	ConcurrentIndexes = 20
-)
-
-type CrawlerError string
-
-func (e CrawlerError) Error() string {
-	return "Crawler: " + string(e)
-}
-
-// Construct a new web crawler.
-func NewCrawler(con containers.Container, db Storage) (c *Crawler, err error) {
-	defer func() {
-		if lErr := recover(); lErr != nil {
-			err = ErrNewCrawler
-			c = &Crawler{}
-		}
-	}()
-	c = new(Crawler)
-	c.urlList = con
-	c.db = db
-	c.robots = make(map[string]*robotstxt.RobotsData, RobotsSize)
-	c.links = make(chan string)
+func NewCrawler(storage Storage) (c *Crawler, err error) {
+	c = &Crawler{}
+	c.MaxRequests = 10
+	c.MaxConcurrentRequests = 1
+	c.db = storage
 	err = c.db.OpenConnection()
 	if err != nil {
 		return nil, err
 	}
+	c.links = make(chan string, c.MaxConcurrentRequests)
 	return
 }
 
-// Add the internal base url to start crawling from.
-func (c *Crawler) AddURL(URL string) (err error) {
-	node, err := c.urlList.AddNode(URL)
-	if err != nil {
-		return err
+func (c *Crawler) Crawl(uri string) (err error) {
+	if c.MaxRequests <= 0 || c.MaxConcurrentRequests <= 0 {
+		return ErrInvalidParameters
 	}
-	if node == nil {
-		return ErrAddURL
-	}
-	return
-}
-
-// Remove a URL from the container.
-func (c *Crawler) RemoveURL(key string) (err error) {
-	err = c.urlList.RemoveNode(key)
-	return err
-}
-
-// Retrieve the HTML content of a url with index i in the Crawler.
-func (c *Crawler) RetrieveHTML(key string) (result string, err error) {
-	resp, err := http.Get(key)
-	if err != nil {
-		return
-	}
-	defer resp.Body.Close()
-	var b []byte
-	b, err = ioutil.ReadAll(resp.Body)
-	result = string(b)
-	return
-}
-
-// The main function that extracts various information for a url and its page.
-func (c *Crawler) ExtractInfo(key string) (err error) {
-	if !c.CheckRobots(key) {
-		return ErrAccessDenied
-	}
-
-	resp, err := http.Get(key)
-	if err != nil {
-		return err
-	}
-	defer resp.Body.Close()
-	err = c.IndexURL(key)
-	if err != nil {
-		return err
-	}
-	err = c.Index(resp)
-	if err != nil {
-		return err
+	go func() {
+		c.links <- uri
+	}()
+	count := 0
+L:
+	for {
+		if count >= c.MaxRequests {
+			break
+		}
+		select {
+		case link, ok := <-c.links:
+			count++
+			if ok {
+				go c.extractInfo(link)
+			} else {
+				fmt.Println("everything closed")
+			}
+		case <-time.After(5 * time.Second):
+			break L
+		}
 	}
 	return nil
 }
 
-// Split a URL into tokens and index them.
-func (c *Crawler) IndexURL(u string) (err error) {
-	parsed_url, err := url.Parse(u)
+func (c *Crawler) extractInfo(link string) {
+	defer func() {
+		if err := recover(); err != nil {
+			fmt.Printf("could not retrieve %s: %s\n", link, err)
+		}
+	}()
+	fmt.Printf("retrieving %s\n", link)
+	client := &http.Client{
+		Timeout: time.Second * 5,
+	}
+	resp, err := client.Get(link)
 	if err != nil {
-		return
+		panic(err)
 	}
-	urls, err := containers.TokenizeURL(parsed_url)
-	if err != nil {
-		return
-	}
-	for i := range urls {
-		c.db.InsertRecord(urls[i], u, "urlindex")
-	}
-	return
+	defer resp.Body.Close()
+	c.index(resp)
 }
 
-// Index all words and checksummed links in the document and record them.
-func (c *Crawler) Index(resp *http.Response) (err error) {
+func (c *Crawler) index(resp *http.Response) (err error) {
 	z := html.NewTokenizer(resp.Body)
 	insideBody := false
-	key := resp.Request.URL
-
+L:
 	for {
 		tt := z.Next()
 		tok := z.Token()
 		switch {
 		case tt == html.ErrorToken:
-			// end of document
-			return nil
+			break L
 		case tt == html.StartTagToken:
 			if tok.Data == "body" {
-				// We can start recording words.
 				insideBody = true
 			} else if tok.Data == "a" {
-				err := c.indexLinks(&tok, key)
-				if err != nil {
-					return err
+				for i := range tok.Attr {
+					err := c.indexLinks(tok.Attr[i].Val, resp.Request.URL)
+					if err != nil {
+						return err
+					}
 				}
 			}
 		case tt == html.EndTagToken:
-			// Stop recording words.
 			if tok.Data == "body" {
 				insideBody = false
 			}
 		case tt == html.TextToken && insideBody:
-			// We are still inside the body and we have encountered a text token, which
-			// means we are finding all words inside tags.
-			err = c.indexWords(&tok, resp.Request.URL.String())
-			if err != nil {
-				return err
-			}
-		}
-	}
-}
-
-// Extract the ref from a link and record it after checksumming. Also add
-// it to the buffer.
-func (c *Crawler) indexLinks(tok *html.Token, key *url.URL) (err error) {
-	for i := range tok.Attr {
-		if tok.Attr[i].Key == "href" {
-			val, err := url.Parse(tok.Attr[i].Val)
-			if err != nil {
-				continue
-			}
-			if !val.IsAbs() {
-				val.Host = key.Host
-				val.Scheme = key.Scheme
-			}
-			// Add to buffer.
-			c.links <- val.String()
-			// Don't add to buffer, add it to a channel and return that
-			sum := md5.Sum([]byte(val.String()))
-			cs := hex.EncodeToString(sum[:])
-			err = c.db.InsertRecord(cs, key.String(), "linkindex")
-			if err != nil {
-				return err
-			}
+			//
 		}
 	}
 	return nil
 }
 
-// Extract all words in a body and record them.
-func (c *Crawler) indexWords(tok *html.Token, u string) (err error) {
-	reg := regexp.MustCompile(`\w+`)
-	if !IsIgnored(tok.Data) {
-		words := reg.FindAllString(tok.Data, -1)
-		for i := range words {
-			err = c.db.InsertRecord(words[i], u, "wordindex")
-			if err != nil {
-				return err
-			}
-		}
-	}
-	return nil
-}
-
-// Check if the word we're checking is in the ignored fragments-list.
-func IsIgnored(str string) bool {
-	str = strings.ToLower(str)
-	if len(str) == 0 {
-		return true
-	}
-	for i := range SkippedFragments {
-		if str == SkippedFragments[i] {
-			return true
-		}
-	}
-	return false
-}
-
-// Check for permissions with robots.txt.
-func (c *Crawler) CheckRobots(u string) bool {
-	// Get hostname
-	URL, err := url.Parse(u)
+func (c *Crawler) indexLinks(uri string, key *url.URL) (err error) {
+	u, err := url.Parse(uri)
 	if err != nil {
-		return false
+		return err
 	}
-	var (
-		val *robotstxt.RobotsData
-		ok  bool
-	)
-	if val, ok = c.robots[URL.Host]; !ok {
-		// Does not exist, add to map
-		// But first check if map isn't full
-		if len(c.robots) > RobotsSize {
-			// For now discard
-			// Discarding has the current added benefit that newer robots.txt
-			// are retrieved again after discarding, so we don't have to check for
-			// updates. Either way, might not be the best way to do this.
-			c.robots = make(map[string]*robotstxt.RobotsData, RobotsSize)
-		}
-		r, err := http.Get("http://" + URL.Host + "/robots.txt")
-		if err != nil {
-			return false
-		}
-		rt, err := robotstxt.FromResponse(r)
-		if err != nil {
-			return false
-		}
-		c.robots[URL.Host] = rt
-		val = c.robots[URL.Host]
+	if !u.IsAbs() {
+		u.Host = key.Host
+		u.Scheme = key.Scheme
 	}
-	return val.TestAgent(URL.Path, "MIR")
-}
-
-func (c *Crawler) Crawl(first string) {
-	// go func() {
-	// 	c.links <- first
-	// }()
-	// for link := range c.links {
-	// 	go func() {
-	// 		_, err := c.urlList.AddNode(link)
-	// 		if err == nil {
-	// 			fmt.Println("indexing", link)
-	// 			c.ExtractInfo(link)
-	// 		}
-	// 	}()
-	// }
-	go func() {
-		c.links <- first
-	}()
-	num := 0
-	for link := range c.links {
-		num++
-		if num == 30 {
-			break
-		}
-		go func() {
-			fmt.Printf("indexing %s\n", link)
-			c.ExtractInfo(link)
-		}()
+	sum := md5.Sum([]byte(u.String()))
+	cs := hex.EncodeToString(sum[:])
+	err = c.db.InsertRecord(cs, key.String(), "linkindex")
+	if err != nil {
+		return err
 	}
+	return nil
 }
--- sprinter/crawl_test.go
+++ sprinter/crawl_test.go
@@ -1,229 +1,46 @@
 package sprinter_test
 
-import (
-	"bytes"
-	"errors"
-	"fmt"
-	"io"
-	"net/http"
-	"net/url"
-	"strings"
-	"testing"
+import "testing"
+import . "github.com/Nvveen/mir/sprinter"
 
-	"github.com/Nvveen/mir/containers"
-	. "github.com/Nvveen/mir/sprinter"
-)
+type mockStorage map[string]map[string]string
 
-// TODO check all Exported functions, and see if they can be made private
-
-var (
-	errInvalidDBElement = errors.New("Invalid Database element")
-
-	u = &url.URL{
-		Scheme:   "http",
-		Opaque:   "",
-		User:     nil,
-		Host:     "www.leidenuniv.nl",
-		Path:     "",
-		RawQuery: "",
-		Fragment: "",
+func newMockStorage() *mockStorage {
+	m := &mockStorage{
+		"linkindex": map[string]string{},
 	}
-	link = "http://www.liacs.nl"
-
-	c *Crawler
-	m *mockStorage
-)
-
-type mockStorage map[string]map[string][]string
-
-func (m *mockStorage) OpenConnection() error {
-	return nil
+	return m
 }
 
 func (m *mockStorage) CloseConnection() {
 }
 
+func (m *mockStorage) OpenConnection() (err error) {
+	return nil
+}
+
 func (m *mockStorage) InsertRecord(key string, url string, collection string) (err error) {
-	defer func(err *error) {
-		if r := recover(); r != nil {
-			*err = errors.New("could not insert into mock storage")
-		}
-	}(&err)
-	// ignore duplicates
-	for i := range (*m)[collection][key] {
-		if (*m)[collection][key][i] == url {
-			return nil
-		}
-	}
-	(*m)[collection][key] = append((*m)[collection][key], url)
+	(*m)[collection][key] = url
 	return nil
-}
-
-func (m mockStorage) String() string {
-	buf := new(bytes.Buffer)
-	for col, _ := range m {
-		fmt.Fprintf(buf, "%s:\n", col)
-		for key, val := range m[col] {
-			fmt.Fprintf(buf, "\t%s: ", key)
-			for i := range val {
-				if i < len(val)-1 {
-					fmt.Fprintf(buf, "%s, ", val[i])
-				} else {
-					fmt.Fprintf(buf, "%s\n", val[i])
-				}
-			}
-		}
-	}
-	return buf.String()
-}
-
-// We need to wrap a stringbuffer to be a ReadCloser
-type strBufCloser struct {
-	io.Reader
-}
-
-func (s strBufCloser) Close() error {
-	return nil
-}
-
-func makeCrawler(t *testing.T) (cr *Crawler) {
-	if m == nil {
-		m = new(mockStorage)
-	}
-	*m = mockStorage{
-		"urlindex":  map[string][]string{},
-		"linkindex": map[string][]string{},
-		"wordindex": map[string][]string{},
-	}
-	if c == nil {
-		cr, err := NewCrawler(&containers.List{}, m)
-		if err != nil {
-			t.Fatal(err)
-		}
-		err = cr.AddURL(u.String())
-		if err != nil {
-			t.Fatal(err)
-		}
-		c = cr
-	}
-	return c
 }
 
 func TestNewCrawler(t *testing.T) {
-	// No need for making a crawler with the correct mockStorage, as we
-	// won't be using any functionality in this test.
-	l := &containers.List{}
-	_, err := NewCrawler(l, &mockStorage{})
+	c, err := NewCrawler(newMockStorage())
 	if err != nil {
 		t.Fatal(err)
 	}
-	b := &containers.BinaryTree{}
-	_, err = NewCrawler(b, &mockStorage{})
-	if err != nil {
-		t.Fatal(err)
-	}
-}
-
-func TestSetURL(t *testing.T) {
-	c, err := NewCrawler(&containers.List{}, &mockStorage{})
-	if err != nil {
-		t.Fatal(err)
-	}
-	err = c.AddURL("http://www.google.com")
-	if err != nil {
-		t.Fatal(err)
-	}
-}
-
-func TestRetrieveHTML(t *testing.T) {
-	c := makeCrawler(t)
-	result, err := c.RetrieveHTML(u.String())
-	if err != nil {
-		t.Fatal(err)
-	} else if len(result) == 0 {
-		t.Fatal("no response from " + u.String())
-	}
-}
-
-func TestCrawler_ExtractInfo(t *testing.T) {
-	c := makeCrawler(t)
-	err := c.ExtractInfo(u.String())
-	if err != nil {
-		t.Fatal(err)
-	}
-}
-
-func TestCrawler_IndexURL(t *testing.T) {
-	c := makeCrawler(t)
-	err := c.IndexURL(u.String())
-	if err != nil {
-		t.Fatal(err)
-	}
-}
-
-// NOTE
-// If I want to split storage from sprinter, I could just move this to the
-// mongo testing while importing the crawler, instead of the other way around.
-
-func TestCrawler_RealStorage(t *testing.T) {
-	if testing.Short() {
-		t.Skip("skipping mongo storage testing for the crawler")
-	}
-	// This could be replaced with TestDB?
-	db := &MongoDB{
-		Host:     "127.0.0.1",
-		Port:     "40001",
-		Database: "gotest",
-	}
-	c, err := NewCrawler(&containers.List{}, db)
-	if err != nil {
-		t.Fatal(err)
-	}
-	err = c.IndexURL("http://www.leidenuniv.nl")
-	if err != nil {
-		t.Fatal(err)
-	}
-}
-
-func TestCrawler_Index(t *testing.T) {
-	body := `<html><head><title></title></head><body>
-<a href="http://www.liacs.nl">liacs hoi</a>bla<p>wat</p></body></html>`
-	c := makeCrawler(t)
-	strBuf := strings.NewReader(body)
-	resp := &http.Response{
-		Body:    strBufCloser{strBuf},
-		Request: &http.Request{URL: u},
-	}
-	err := c.Index(resp)
-	if err != nil {
-		t.Fatal(err)
-	}
-}
-
-func TestCrawler_CheckRobots(t *testing.T) {
-	if testing.Short() {
-		t.Skip("skipping because test does a HTTP request to an external site")
-	}
-	c := makeCrawler(t)
-	u := "http://www.google.com"
-	c.AddURL(u)
-	if !c.CheckRobots(u) {
-		t.Fatal("http://www.google.com should be indexable")
-	}
-	if c.CheckRobots(u + "/search") {
-		t.Fatal("http://www.google.com/search should not be indexable")
-	}
+	_ = c
 }
 
 func TestCrawler_Crawl(t *testing.T) {
-	m := &mockStorage{
-		"urlindex":  map[string][]string{},
-		"linkindex": map[string][]string{},
-		"wordindex": map[string][]string{},
+	if testing.Short() {
+		t.Skip("skipping because of http request")
 	}
-	c, err := NewCrawler(&containers.BinaryTree{}, m)
+	m := newMockStorage()
+	c, err := NewCrawler(m)
 	if err != nil {
 		t.Fatal(err)
 	}
 	c.Crawl("http://www.liacs.nl")
+	t.Logf("%#v\n", m)
 }
