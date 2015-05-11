fa76b508770562065ee3cbe253aa14972cd85077
Neal van Veen <nealvanveen@gmail.com>
2015-05-11 12:06:03 +0200

    add more benchmarks

--- sprinter/benchmarks_test.go
+++ sprinter/benchmarks_test.go
@@ -16,8 +16,63 @@ func BenchmarkCrawl_SequentialStaticList(b *testing.B) {
 	if err != nil {
 		b.Fatal(err)
 	}
-	c.MaxRequests = 1000
+	c.MaxRequests = 2000
 	c.MaxConcurrentRequests = 1
+	b.ResetTimer()
+	c.CrawlSequential("http://localhost:8080")
+}
+
+func BenchmarkCrawl_Concurrent10StaticList(b *testing.B) {
+	c, err := NewCrawler(newMockStorage(), &containers.List{})
+	if err != nil {
+		b.Fatal(err)
+	}
+	c.MaxRequests = 2000
+	c.MaxConcurrentRequests = 10
+	b.ResetTimer()
+	c.Crawl("http://localhost:8080")
+}
+
+func BenchmarkCrawl_Concurrent100StaticList(b *testing.B) {
+	c, err := NewCrawler(newMockStorage(), &containers.List{})
+	if err != nil {
+		b.Fatal(err)
+	}
+	c.MaxRequests = 2000
+	c.MaxConcurrentRequests = 100
+	b.ResetTimer()
+	c.Crawl("http://localhost:8080")
+}
+
+func BenchmarkCrawl_Concurrent1000StaticList(b *testing.B) {
+	c, err := NewCrawler(newMockStorage(), &containers.List{})
+	if err != nil {
+		b.Fatal(err)
+	}
+	c.MaxRequests = 2000
+	c.MaxConcurrentRequests = 1000
+	b.ResetTimer()
+	c.Crawl("http://localhost:8080")
+}
+
+func BenchmarkCrawl_Concurrent1000MongoList(b *testing.B) {
+	c, err := NewCrawler(TestDB, &containers.List{})
+	if err != nil {
+		b.Fatal(err)
+	}
+	c.MaxRequests = 2000
+	c.MaxConcurrentRequests = 1000
+	b.ResetTimer()
+	c.Crawl("http://localhost:8080")
+}
+
+func BenchmarkCrawl_Concurrent1000MongoBST(b *testing.B) {
+	c, err := NewCrawler(TestDB, &containers.BinaryTree{})
+	if err != nil {
+		b.Fatal(err)
+	}
+	c.MaxRequests = 2000
+	c.MaxConcurrentRequests = 1000
 	b.ResetTimer()
 	c.Crawl("http://localhost:8080")
 }
